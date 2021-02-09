4c4467b7336dfa8db641978a50a45ab8f944f533
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-02-09 22:28:07 +0300

    Add registration for private user model

--- server/models.py
+++ server/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class PrivateUser(models.Model):
+    username = models.CharField('Ник пользователя', max_length=50)
+    password_hash = models.CharField('Хэш пароля пользователя', max_length=77)
--- server/views.py
+++ server/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from server.models import PrivateUser
 
-# Create your views here.
-def index(request):
-    return HttpResponse('Hello World!')
+
+def register_user(request):
+    private_registration = can_register_private(request)
+    public_registration = can_register_public(request)
+    if private_registration.status_code != 200:
+        return private_registration
+    if public_registration.status_code != 200:
+        return public_registration
+    return HttpResponse('OK')
+
+
+def can_register_private(request):
+    if request.method == 'POST' and 'username' in request.POST and 'password' in request.POST:
+        if request.POST['username'] > 50:
+            return HttpResponseBadRequest('username is too long - 50 symbols is limit')
+        if request.POST['username'] in PrivateUser.username:
+            return HttpResponseBadRequest('username is already in use')
+        HttpResponse('OK')
+    else:
+        HttpResponseBadRequest('bad request format')

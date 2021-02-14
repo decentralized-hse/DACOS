a517465a38706ceeb74c6fe52b16070da6472753
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-02-14 17:45:54 +0300

    Final version of registration

--- server/views.py
+++ server/views.py
@@ -1,7 +1,8 @@
+from django.core import serializers
+from argon2 import PasswordHasher
 from server.models import PrivateUser
-from django.http import HttpResponse,HttpResponseNotAllowed,HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
 from .models import PublicUser
-
 
 """
 register user for both PrivateUser and PublicUser tables
@@ -13,8 +14,10 @@ def register_user(request):
     if private_registration.status_code != 200:
         return private_registration
     if public_registration.status_code != 200:
-
         return public_registration
+    PrivateUser(username=request.POST['username'], password_hash=PasswordHasher().hash(request.POST['password'])).save()
+    PublicUser(username=request.POST['username'], cell_id=1, public_rsa_n=request.POST['public_rsa_n'],
+               public_rsa_e=request.POST['public_rsa_e'], g_in_big_power=request.POST['g_in_big_power']).save()
     return HttpResponse('OK')
 
 
@@ -32,7 +35,7 @@ def can_register_private(request):
 def can_register_public(request):
     if request.method != 'POST':
         return HttpResponseNotAllowed('Invalid request type')
-    if 'username' in request.POST  and 'public_rsa_n' in request.POST and'public_rsa_e' in request.POST and 'g_in_big_power' in request.POST:
+    if 'username' in request.POST and 'public_rsa_n' in request.POST and 'public_rsa_e' in request.POST and 'g_in_big_power' in request.POST:
         if len(request.POST.get('username')) > 50:
             return HttpResponseBadRequest('username is too long - 50 symbols is limit')
         if check_username(request.POST.get('username'), PublicUser):

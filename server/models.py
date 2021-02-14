a925baf15aa8ab12f3e9728d766950fd34bcfe0d
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-02-14 15:39:15 +0300

    Fixed files to PEP8

--- server/models.py
+++ server/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class PublicUser(models.Model):
-    username = models.CharField(max_length = 50)
+    username = models.CharField(max_length=50)
     cell_id = models.IntegerField()
-    public_rsa_n = models.CharField(max_length = 300)
-    public_rsa_e = models.CharField(max_length = 300)
-    g_in_big_power = models.CharField(max_length = 300)
+    public_rsa_n = models.CharField(max_length=300)
+    public_rsa_e = models.CharField(max_length=300)
+    g_in_big_power = models.CharField(max_length=300)
 
 
 class PrivateUser(models.Model):

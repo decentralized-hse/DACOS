a7dd6016306ee00d037968d1ad7679e1da8c115b
Lokhmat <lokhmatikov.htc@gmail.com>
2021-02-14 15:07:57 +0300

    Changed fields types

--- server/models.py
+++ server/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class PublicUser(models.Model):
     username = models.CharField(max_length = 50)
     cell_id = models.IntegerField()
-    public_rsa_n = models.IntegerField()
-    public_rsa_e = models.IntegerField()
-    g_in_big_power = models.IntegerField()
+    public_rsa_n = models.CharField(max_length = 300)
+    public_rsa_e = models.CharField(max_length = 300)
+    g_in_big_power = models.CharField(max_length = 300)
 
     

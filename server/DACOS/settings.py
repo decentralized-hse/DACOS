3103f026c7f380b63ec28af7109e74c24e82ce52
Lokhmat <lokhmatikov.htc@gmail.com>
2021-03-07 21:03:23 +0300

    First step to registration

--- server/DACOS/settings.py
+++ server/DACOS/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '10.0.2.2',
+    '127.0.0.1'
+]
 
 
 # Application definition

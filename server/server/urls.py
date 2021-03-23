97aa236af5311a8e3fc31027a2b8531afdccc66a
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-03-23 20:36:54 +0300

    Minor fix

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register', views.register_user),
-    path('delete_ticket', views.delete_ticket),
-    path('add_ticket', views.register_ticket),
-    path('get_tickets', views.get_all_tickets),
-    path('read_message', views.read_message),
-    path('get_tickets', views.get_all_tickets),
     path('write_msg', views.write_log),
 ]

5af534bff27dcd18baddda0c53ae7d6a50f6f632
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-03-31 16:34:42 +0300

    Change min sdk to 24 to use stream API in code

---
+++ client/app/src/main/java/com/teama/dacosclient/services/GetNewUsersService.java
@@ -0,0 +1,16 @@
+package com.teama.dacosclient.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class GetNewUsersService extends Service {
+    public GetNewUsersService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

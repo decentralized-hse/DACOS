da84afa5387701dfa0497cafef9aa9582966ae3b
Kocheshkov Dmitrii <dimakid123@gmail.com>
2022-05-19 03:11:00 +0300

    Change models used in migrations

--- server/server/models.py
+++ server/server/models.py
@@ -3,24 +3,21 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class PublicUser(models.Model):
-    username = models.CharField(help_text='Ник пользователя, дублирован в PrivateUser', max_length=50)
-    # TODO: Maybe should start containing public key as hex string, not as byte array.
-    public_key = ArrayField(models.IntegerField(),
-                            help_text='Публичный ключ пользователя, используется для шифрования сообщения конкретному'
-                                      ' пользователю. Применяется криптография на эллиптических кривых. Хранится в виде'
-                                      ' массива байт длинной 32.', )
+    username = models.TextField(help_text='Ник пользователя, дублирован в PrivateUser')
+    public_key = models.TextField(help_text='Публичный ключ пользователя, используется для шифрования'
+                                            ' сообщения конкретному'
+                                            ' пользователю. Применяется RSA 2048')
+    register_server = models.TextField(help_text='Адрес сервера на котором пользователь зарегистрирован.'
+                                                 ' Хранится в виде address:port')
 
 
-class PrivateUser(models.Model):
-    username = models.CharField(help_text='Ник пользователя, дублирован в PublicUser', max_length=50)
-    password_hash = models.CharField(help_text='Хэш пароля пользователя для проверки авторизации', max_length=77)
-
-
-class Block(models.Model):
-    block = ArrayField(models.CharField(max_length=500), size=32,
-                       help_text='Блоки зашифрованных сообщений, размер блока можно поменять в settings.py '
-                                 '(максимальный размер блока - 32).')
+class EncodedMessage(models.Model):
+    models.TextField('')
+    text = ArrayField(models.CharField(max_length=500), size=32,
+                      help_text='Текст зашифрованного сообщения.')
 
 
 class Server(models.Model):
-    url = models.CharField(help_text="Url + port сервера, на котором также запущен сервер DACOS.", max_length=100)
+    url = models.CharField(help_text="Url:port сервера, на котором также запущен сервер DACOS.", max_length=100)
+    public_key = models.TextField(help_text='Публичный ключ сервера, используется для шифрования'
+                                            ' сообщения для пересылки на сервер. Применяется RSA 2048')
